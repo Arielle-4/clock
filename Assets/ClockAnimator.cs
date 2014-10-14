279ce732158221dd89b742b4f37de10822a73781
Arielle-4 <arielle-4@hotmail.com>
2014-10-14 17:39:19 +0200

    initial commit

---
+++ Assets/ClockAnimator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine; // we use namespaces called unityEngine and System
+using System;// the namespace contains classes
+
+public class ClockAnimator : MonoBehaviour { // the class clockAnimator inherits from monobehavior 
+
+
+
+	private const float   // constant variables declared ( variables that dont change)
+		hoursToDegree = 360/12f, 
+		minutesToDegree = 360/60f,
+		secondsToDegree = 360/60f;
+
+	public Transform hours, minutes, seconds;
+
+	public bool analog;
+
+
+	void Update () {
+
+		if (analog) {
+
+		}
+
+		else {
+
+		DateTime time = DateTime.Now;
+
+		hours.localRotation = Quaternion.Euler(0f,0f,time.Hour* - hoursToDegree);
+		minutes.localRotation = Quaternion.Euler(0f,0f,time.Minute* - minutesToDegree);
+		seconds.localRotation = Quaternion.Euler(0f,0f,time.Second* - secondsToDegree);
+
+		
+		}
+	
+	}
+
+
+}
